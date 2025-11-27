6f9ab9ec42bf4ab4612dfec6b45b9c32391659ad
Karim de Fombelle <karim.defombelle@gmail.com>
2025-11-27 23:23:41 +0800

    linear regression based on geolocalised valeurs foncieres

---
+++ README.md
@@ -0,0 +1,3 @@
+cd dataset
+tar -xvf full.tar.gz && mv full.csv valeurs-foncieres-2005.csv
+cd ..
--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from sklearn import preprocessing
 parser = argparse.ArgumentParser(description='Process some data')
 parser.add_argument('input_file', help='Input file name, should be a .txt as per data.gouv.fr valeurs foncieres datasets')
 args = parser.parse_args()
-print(f"Input: {args.input_file}")
+print(f"Input: [{args.input_file}]")
 input_file=args.input_file
 output_file_name, extension=os.path.splitext(args.input_file)
-output_file=output_file_name+".csv"
+output_file=output_file_name+"-cleansed.csv"
 
 print("["+input_file + "] will be prepared into ["+output_file+"]")
 
@@ -22,25 +22,25 @@ logging.basicConfig(
 )
 
 #https://www.data.gouv.fr/datasets/demandes-de-valeurs-foncieres/#/resources/d573456c-76eb-4276-b91c-e6b9c89d6656
-df = pd.read_csv('./'+input_file, sep='|', on_bad_lines='skip')
+df = pd.read_csv('./'+input_file, on_bad_lines='skip')
 print("INPUT")
-print(df.info())
+print(df.columns.to_list())
 
 #filter the lines of interest
 df = df[(
-            (df['Type local'] == 'Appartement')
+            (df['type_local'] == 'Appartement')
             #& (df['Commune'] == 'NICE')
-            & (df['Nature mutation'] == 'Vente')
+            & (df['nature_mutation'] == 'Vente')
             #& (df['Code departement'] != '2A')
             #& (df['Code departement'] != '2B')
         )]
 
 #drop the columns not used
 exclusions = [
-    #'id_mutation',
-    #'date_mutation',
+    'id_mutation',
+    'date_mutation',
     'numero_disposition',
-    #'nature_mutation',
+    'nature_mutation',
     #'valeur_fonciere',
     'adresse_numero',
     'adresse_suffixe',
@@ -78,13 +78,37 @@ exclusions = [
     #'longitude',
     #'latitude'
  ]
-df = df.drop(columns=exclusions)
+df.drop(columns=exclusions, inplace=True)
 
-#drop NaN
-df.dropna(inplace=True)
 #drop DOM-TOM
 prefixes = ('97', '988')
 df = df[~df['code_postal'].astype(str).str.startswith(prefixes)]
+
+#add index
+df.reset_index(names='Index', inplace=True)
+
+print(df.isna().sum())
+print("---------")
+
+#data preparation for encoding
+#df['Section'] = df['Section'].astype('category')
+#df['Commune'] = df['Commune'].astype('string')
+#df['Valeur fonciere'] = df['Valeur fonciere'].str.replace(',', '.').astype(float)
+mask = df["code_commune"].str.startswith("2A", na=False)
+df.loc[mask, "code_commune"] = "11111"
+mask = df["code_commune"].str.startswith("2B", na=False)
+df.loc[mask, "code_commune"] = "22222"
+#df['code_commune'] = df['code_commune'].str.replace(r"^2B.*", "22222", regex=True)
+#df['Code departement'] = df['Code departement'].astype(int)
+
+print(df.isna().sum())
+print("---------")
+
+#drop NaN
+df.dropna(inplace=True)
+
+print(df.isna().sum())
+print("---------")
 
 #dataset sanity checks
 if df.isna().any().any():
@@ -97,22 +121,13 @@ if df.isnull().any().any():
 else:
     logging.info("No null values")
 
-#add index
-df.reset_index(names='Index', inplace=True)
-
-#data preparation for encoding
-df['Section'] = df['Section'].astype('category')
-df['Commune'] = df['Commune'].astype('string')
-df['Valeur fonciere'] = df['Valeur fonciere'].str.replace(',', '.').astype(float)
-df['Code departement'] = df['Code departement'].replace({'2A': 2001, '2B': 2002})
-df['Code departement'] = df['Code departement'].astype(int)
-
 print("OUTPUT")
 print(df.info())
 
 #standardize the data (xi-avg(X1)/stdev
-df[['Surface reelle bati']] = preprocessing.scale(df[['Surface reelle bati']])
-df[['Nombre pieces principales']] = preprocessing.scale(df[['Nombre pieces principales']])
+df[['surface_reelle_bati']] = preprocessing.scale(df[['surface_reelle_bati']])
+df[['nombre_pieces_principales']] = preprocessing.scale(df[['nombre_pieces_principales']])
+df[['surface_terrain']] = preprocessing.scale(df[['surface_terrain']])
 print(df.head())
 
 logging.info("Cleansed data written in " + output_file)
